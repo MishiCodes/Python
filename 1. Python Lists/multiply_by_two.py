ce26bbf24fb12fb29322e8d2ee0477589c688f92
Mehwish Roohan <53234237+MishiCodes@users.noreply.github.com>
2019-12-05 03:30:47 +0100

    Update multiply_by_two.py

--- 1. Python Lists/multiply_by_two.py
+++ 1. Python Lists/multiply_by_two.py
@@ -1,3 +1,22 @@
 numbers = [1, 2, 3, 4, 5]
-new_list = [n*2 for n in numbers]  # using list comprehension
-print(new_list)
+
+# --------- Method 1: Using list comprehension-------------
+my_list = [n*2 for n in numbers]  
+print(my_list)
+
+# ---------- Method 2: Using map()--------------------------
+def multiply_by_two(number):
+    return number * 2
+
+my_list = map(multiply_by_two, numbers)
+print(list(my_list)) # convert the map object into a list, for readability
+
+# ---------- Method 3: Using a map + lambda------------------
+my_list = map(lambda n: n*2, numbers)
+print(list(my_list))
+
+# ---------- Method 4: Using list append with for loop--------
+my_list = []
+for n in numbers:
+    my_list.append(n*2)
+print(my_list)
