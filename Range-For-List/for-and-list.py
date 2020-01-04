be537b5fe80aa06d5c48fa52e17d5077fc8f6b1a
Mehwish Roohan <53234237+MishiCodes@users.noreply.github.com>
2020-01-04 02:58:16 +0100

    for-loop

---
+++ Range-For-List/for-and-list.py
@@ -0,0 +1,25 @@
+for item in 'Python':
+    print(item)
+print("----------------------")
+
+for item in ['Mehwish', 'Sara','Ali']:
+    print (item)
+print("----------------------")
+
+for item in range(10):
+    print (item)
+print("----------------------")
+
+for item in range(5,10):
+    print (item)
+print("----------------------")
+
+for item in range(5,10,2):
+    print (item)
+print("----------------------")
+
+# for coordinates
+for x in range(4):
+    for y in range(3):
+        print(f'({x},{y})')
+
